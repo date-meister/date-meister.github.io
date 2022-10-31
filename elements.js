d47fec545e157935a9897d3dbd5dc7a7fe9bead6
Danny Engelman <danny@engelman.nl>
2022-10-31 14:39:58 +0100

    refactored console output

--- elements.js
+++ elements.js
@@ -1,73 +1,91 @@
 const cmp_date = "full-date";
 const cmp_clock = "day-clock";
 const cmp_count = "day-count";
-const cmp_since = "period-since";
-const cmp_to = "period-to";
+const named = (s) => `<` + s + `>`;
+// ================================================================
+const fullDate = (
+  date,
+  locale,
+  options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+) => date.toLocaleDateString(locale, options);
+// ================================================================
+const dayClock = (locale, seconds, scope) =>
+  new Date()
+    .toLocaleTimeString(
+      locale,
+      seconds
+        ? {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          }
+        : { timeStyle: "short" }
+    )
+    .split(":")
+    .join(
+      `<span style="opacity:${
+        seconds ? 1 : (scope.show = !scope.show) ? 1 : 0
+      }">:</span>`
+    );
+// ================================================================
+const dayCount = (scope) =>
+  ~~(
+    (new Date(scope.getAttribute("to"))?.getTime() - scope.date?.getTime()) /
+    (1000 * 3600 * 24)
+  );
+// ================================================================
 console.log(
   "%c Date Web Components: ",
   "background:green;color:yellow",
-  cmp_date,
-  cmp_clock,
-  cmp_count,
-  cmp_since,
-  cmp_to
+  "\n",
+  named(cmp_date),
+  fullDate(new Date(), navigator.language),
+  "\n",
+  named(cmp_clock),
+  dayClock(navigator.language, true),
+  "\n",
+  named(cmp_count),
+  dayCount(document.body)
 );
+// ================================================================
 class DateComponentClass extends HTMLElement {
   connectedCallback() {
     this.style.display = "inline-block";
-    this.locale = this.getAttribute("locale") || "nl-NL"; //"en-US";
+    this.locale = this.getAttribute("locale") || navigator.language; //"nl-NL"; //"en-US";
     this.date = new Date(this.getAttribute("date") || Date.now());
     this.render();
   }
 }
+// ================================================================
 customElements.define(
   cmp_date,
   class extends DateComponentClass {
-    render(
-      options = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }
-    ) {
-      this.innerHTML = this.date.toLocaleDateString(this.locale, options);
+    render() {
+      this.innerHTML = fullDate(this.date, this.locale);
     }
   }
 );
+// ================================================================
 customElements.define(
   cmp_clock,
   class extends DateComponentClass {
     render(seconds = false) {
-      this.innerHTML = new Date()
-        .toLocaleTimeString(
-          this.locale,
-          seconds
-            ? {
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-              }
-            : { timeStyle: "short" }
-        )
-        .split(":")
-        .join(
-          `<span style="opacity:${
-            seconds ? 1 : (this.show = !this.show) ? 1 : 0
-          }">:</span>`
-        );
+      this.innerHTML = dayClock(this.locale, seconds, this);
       if (!seconds) setTimeout(() => this.render(), 1000);
     }
   }
 );
+// ================================================================
 customElements.define(
   cmp_count,
   class extends DateComponentClass {
     render() {
-      this.innerHTML = ~~(
-        (new Date(this.getAttribute("to")).getTime() - this.date.getTime()) /
-        (1000 * 3600 * 24)
-      );
+      this.innerHTML = dayCount(this);
     }
   }
 );
